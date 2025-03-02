b6b016e17054aa2ac30a0441c8177cc222911d2f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-03-01 20:53:18 -0500

    spelling: available
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- filtering/type-based/accent-neutralise.mjs
+++ filtering/type-based/accent-neutralise.mjs
@@ -27,7 +27,7 @@ let $ = jQuery;
  */
 function removeAccents(data) {
     if (data.normalize) {
-        // Use I18n API if avaiable to split characters and accents, then remove
+        // Use I18n API if available to split characters and accents, then remove
         // the accents wholesale. Note that we use the original data as well as
         // the new to allow for searching of either form.
         return data + ' ' + data.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
